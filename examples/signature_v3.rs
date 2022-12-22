7f9ff5e58dda935fd8b75fe84e2c24e84c29927d
gaomingjun <wisarmy@gmail.com>
2022-12-22 15:32:00 +0800

    optimize example

--- examples/signature_v3.rs
+++ examples/signature_v3.rs
@@ -4,29 +4,38 @@ use tencentcloud_sdk_sms::{
 };
 #[tokio::main]
 async fn main() {
+    // prepare param
+    let secret_id = "your secret_id";
+    let secret_key = "your secret_key";
+    let sms_sdk_app_id = "your sms_sdk_app_id";
+    let template_id = "your template_id";
+    let sign_name = "your sign_name";
+    let region = "ap-guangzhou";
+    let phone_number_set = vec!["+86".to_owned()];
+    let template_param_set = vec!["123456".to_owned()];
     // build client
-    let credential = Credential::new("your secret_id", "your secret_key", None);
-    let client = Client::new(credential, "ap-guangzhou");
+    let credential = Credential::new(secret_id, secret_key, None);
+    let client = Client::new(credential, region);
     // build request
     let request = SendSmsRequest::new(
-        vec!["+86..".to_owned()],
-        "your sms_sdk_app_id",
-        "your template_id",
-        "your sign_name",
-        vec!["your template param".to_owned()],
+        phone_number_set.clone(),
+        sms_sdk_app_id,
+        template_id,
+        sign_name,
+        template_param_set,
     );
     // send
     let response = client.send_sms(request).await;
     // check
     match response {
         Ok(res) => {
-            let phone_number = "+86..";
-            println!(
-                "send {}: {:?}",
-                phone_number,
-                //res,
-                res.check_is_success(phone_number.to_owned())
-            );
+            phone_number_set.into_iter().for_each(|phone_number| {
+                println!(
+                    "send {}: {:?}",
+                    phone_number,
+                    res.check_is_success(phone_number.to_owned())
+                );
+            });
         }
         Err(e) => {
             println!("send error: {}", e);
